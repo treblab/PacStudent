d2ebcf6bd5f0f25789aabddadd1f907fb69abb0e
treblab <albertnguyen2002@hotmail.com>
2023-10-06 12:52:50 +1100

    Created script file to play music on game start

--- PacStudent 2D/Assets/Scripts/Music.cs
+++ PacStudent 2D/Assets/Scripts/Music.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Music : MonoBehaviour
 {
+
+    [SerializeField] private AudioSource introBGM;
+    [SerializeField] private AudioSource ghostBGM;
+
     // Start is called before the first frame update
     void Start()
     {

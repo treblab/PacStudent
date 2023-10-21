d0d3076efaf9f000cfc7b11c6835520f800d57e2
treblab <albertnguyen2002@hotmail.com>
2023-10-21 20:52:42 +1100

    Merge branch 'Feature-Visual' into Master

--- PacStudent 2D/Assets/Scripts/Music.cs
+++ PacStudent 2D/Assets/Scripts/Music.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Music : MonoBehaviour
 {
+
+    [SerializeField] private AudioSource introBGM;
+    [SerializeField] private AudioSource ghostBGM;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        introBGM.enabled = true;
+        ghostBGM.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (introBGM.isPlaying == false)
+        {
+            ghostBGM.enabled = true;
+            ghostBGM.loop = true;
+        }
         
     }
 }

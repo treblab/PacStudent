30ef7fe2cf5b7822b89984a6778f45b6eb9fe116
treblab <albertnguyen2002@hotmail.com>
2023-10-24 22:48:32 +1100

    60% band started - creating game HUD UI
    Added:
    - Timers
    - Score
    - Lives
    - Exit button

---
+++ PacStudent 2D/Assets/Scripts/UIManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadLevelOne()
+    {
+        DontDestroyOnLoad(this);
+        SceneManager.LoadSceneAsync(1);
+    }
+}

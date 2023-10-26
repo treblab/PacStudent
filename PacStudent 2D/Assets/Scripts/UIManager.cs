86a5f338092e54111bad2cf4e6f1bbd7c912cd96
treblab <albertnguyen2002@hotmail.com>
2023-10-26 23:45:43 +1100

    Changes to menu formatting made

--- PacStudent 2D/Assets/Scripts/UIManager.cs
+++ PacStudent 2D/Assets/Scripts/UIManager.cs
@@ -5,9 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    // 60% Band - In game UI Appearance:
+    public RectTransform startScreen;
+    public RectTransform gameHUD;
+    public RectTransform ghost1Canvas;
+    public RectTransform ghost2Canvas;
+    public RectTransform ghost3Canvas;
+    public RectTransform ghost4Canvas;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
+        if (startScreen != null)
+        {
+            startScreen.sizeDelta = new Vector2(Screen.width, Screen.height);
+            // Debug.Log("startScreen sizeDelta called.");
+        }
+        
+        if (gameHUD != null)
+        {
+            gameHUD.sizeDelta = new Vector2(Screen.width, Screen.height);
+            Debug.Log("gameHUD sizeDelta called.");
+        }
+
+        if (ghost1Canvas != null)
+        {
+            ghost1Canvas.sizeDelta = new Vector2(Screen.width, Screen.height);
+        }
         
     }
 

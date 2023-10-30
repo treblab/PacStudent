381d31e2986fbd3ba862634615def97bcb3709b5
treblab <albertnguyen2002@hotmail.com>
2023-10-30 19:32:36 +1100

    Assets refreshed for some reason

---
+++ PacStudent 2D/Assets/Scripts/PacStudent/PacStudent InputManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PacStudentMovement : MonoBehaviour
+{
+    [SerializeField] private GameObject PacStudent;
+
+    private Tweener tweener;
+    float timer;
+    float lastTime;
+    bool isMoving;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tweener = GetComponent<Tweener>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if ((int)timer > lastTime) // every second
+        {
+            lastTime = (int)timer; // update time
+
+            // Move right:
+
+
+            // Move down:
+
+
+            // Move left:
+
+
+            // Move up:
+
+        }
+    }
+}

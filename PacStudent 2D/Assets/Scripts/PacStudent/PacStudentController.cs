351b56d7edfd9b2dcae1810b2c3c08178a43f965
treblab <albertnguyen2002@hotmail.com>
2023-10-31 17:48:24 +1100

    Scripts update
    Changes made:
    -LevelGrid was incorrect, being asymmetrical on the right side. This has been corrected.
    - PSController script was simplified to remove redundancy.
    -Tweener has a check called isTweening() now, to check if PS is lerping.
    - PS movement appears to be working with currInput and lastInput now.

--- PacStudent 2D/Assets/Scripts/PacStudent/PacStudentController.cs
+++ PacStudent 2D/Assets/Scripts/PacStudent/PacStudentController.cs
@@ -5,137 +5,112 @@ using Unity.Services.Analytics;
 using UnityEditor;
 using UnityEngine;
 
-public class PacStudentController: MonoBehaviour
+public class PacStudentController : MonoBehaviour
 {
     [SerializeField] private GameObject PacStudent;
     [SerializeField] private Animator animator;
     [SerializeField] private AudioSource movementAudio;
 
     private Tweener tweener;
-
-    bool isMoving;
+    private LevelGrid levelGrid;
 
     private char lastInput;
-    // private char currentInput;
-    private LevelGrid levelGrid;
-    // bool isLerping;
+    private char currentInput;
 
-    // Start is called before the first frame update
+    // Map of keys and their corresponding directions
+    private Dictionary<char, Vector2> charToDirection = new Dictionary<char, Vector2>
+    {
+        { 'W', Vector2.up },
+        { 'A', Vector2.left },
+        { 'S', Vector2.down },
+        { 'D', Vector2.right }
+    };
+
+    // To record KeyCodes as chars
+    private Dictionary<KeyCode, char> keyToChar = new Dictionary<KeyCode, char>
+    {
+        { KeyCode.W, 'W' },
+        { KeyCode.A, 'A' },
+        { KeyCode.S, 'S' },
+        { KeyCode.D, 'D' }
+    };
+
     void Start()
     {
-        isMoving = true;
-
         tweener = GetComponent<Tweener>();
         levelGrid = new LevelGrid();
 
         PacStudent = GameObject.FindWithTag("PacStudent");
-
         animator = PacStudent.GetComponent<Animator>();
-        animator.SetFloat("Horizontal", 0.0f);
-        animator.SetFloat("Vertical", 0.0f);
-
         movementAudio = PacStudent.GetComponent<AudioSource>();
+
         movementAudio.mute = true; // audio muted at the start as PacStudent is idle.
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (!isLerping())
+        {
+            if (!TryMoveInDirection(lastInput))
+            {
+                TryMoveInDirection(currentInput);
+            }
+        }
+
         getPlayerInput();
+    }
+
+    private bool TryMoveInDirection(char direction)
+    {
+        if (charToDirection.ContainsKey(direction))
+        {
+            Vector3 movementVector = levelGrid.TryMove(charToDirection[direction], PacStudent.transform.position);
+            if (movementVector != PacStudent.transform.position) // This checks if the move was valid
+            {
+                MovePacStudent(movementVector, charToDirection[direction]);
+                currentInput = direction;
+                return true;
+            }
+        }
+        return false;
     }
 
     private void getPlayerInput()
     {
-        if (PacStudent != null)
+        if (PacStudent == null) return;
+
+        movementAudio.mute = false;
+        movementAudio.loop = true;
+
+        foreach (var key in keyToChar.Keys)
         {
-            movementAudio.mute = false;
-            movementAudio.loop = true;
-            isMoving = true;
-
-            // W - Move up:
-            if (Input.GetKeyDown(KeyCode.W))
+            if (Input.GetKeyDown(key))
             {
-                // Inverted with Vector2.Down as the grid is inverted when compared to Unity's world coordinates.
-                Vector3 movementVector = levelGrid.TryMove(Vector2.down, PacStudent.transform.position);
-                Vector3 gridToWorldPosVec = gridToWorldPos(movementVector);
-
-                Debug.Log("Movement vector: " + movementVector);
-                Debug.Log("GridToWorld Vec: " + gridToWorldPosVec);
-
-                if (movementVector != PacStudent.transform.position) // This checks if the move was valid
-                {
-                    animator.SetFloat("Horizontal", 0.0f);
-                    animator.SetFloat("Vertical", 1.0f);
-
-                    // Inversion for same reason as before.
-                    tweener.AddTween(PacStudent.transform, PacStudent.transform.position, gridToWorldPosVec, 0.2f);
-                    lastInput = 'W';
-                }
-            }
-
-            // A - Move left:
-            if (Input.GetKeyDown(KeyCode.A))
-            {
-                Vector3 movementVector = levelGrid.TryMove(Vector2.left, PacStudent.transform.position);
-                Vector3 gridToWorldPosVec = gridToWorldPos(movementVector);
-
-                Debug.Log("Movement vector: " + movementVector);
-                Debug.Log("GridToWorld Vec: " + gridToWorldPosVec);
-
-                if (movementVector != PacStudent.transform.position) // This checks if the move was valid
-                {
-                    animator.SetFloat("Horizontal", -1.0f);
-                    animator.SetFloat("Vertical", 0.0f);
-
-                    tweener.AddTween(PacStudent.transform, PacStudent.transform.position, gridToWorldPosVec, 0.2f);
-                    lastInput = 'A';
-                }
-            }
-
-            // S - Move down:
-            if (Input.GetKeyDown(KeyCode.S))
-            {
-                Vector3 movementVector = levelGrid.TryMove(Vector2.up, PacStudent.transform.position);
-                Vector3 gridToWorldPosVec = gridToWorldPos(movementVector);
-
-                Debug.Log("Movement vector: " + movementVector);
-                Debug.Log("GridToWorld Vec: " + gridToWorldPosVec);
-
-                if (movementVector != PacStudent.transform.position) // This checks if the move was valid
-                {
-                    animator.SetFloat("Horizontal", 0.0f);
-                    animator.SetFloat("Vertical", -1.0f);
-
-                    tweener.AddTween(PacStudent.transform, PacStudent.transform.position, gridToWorldPosVec, 0.2f);
-                    lastInput = 'S';
-                }
-            }
-
-            // D - Move right:
-            if (Input.GetKeyDown(KeyCode.D))
-            {
-                Vector3 movementVector = levelGrid.TryMove(Vector2.right, PacStudent.transform.position);
-                Vector3 gridToWorldPosVec = gridToWorldPos(movementVector);
-
-                Debug.Log("Movement vector: " + movementVector);
-                Debug.Log("GridToWorld Vec: " + gridToWorldPosVec);
-
-                if (movementVector != PacStudent.transform.position) // This checks if the move was valid
-                {
-                    animator.SetFloat("Horizontal", 1.0f);
-                    animator.SetFloat("Vertical", 0.0f);
-
-                    tweener.AddTween(PacStudent.transform, PacStudent.transform.position, gridToWorldPosVec, 0.2f);
-                    lastInput = 'D';
-                }
+                lastInput = keyToChar[key];
             }
         }
     }
 
-    // Created as the world pos is negative and grid pos is positive (inverted)
+    private void MovePacStudent(Vector3 targetPosition, Vector2 direction)
+    {
+        Vector3 gridToWorldPosVec = gridToWorldPos(targetPosition);
+        SetAnimatorDirection(direction);
+        tweener.AddTween(PacStudent.transform, PacStudent.transform.position, gridToWorldPosVec, 0.2f);
+    }
+
+    private void SetAnimatorDirection(Vector2 direction)
+    {
+        animator.SetFloat("Horizontal", direction.x);
+        animator.SetFloat("Vertical", direction.y);
+    }
+
     private Vector3 gridToWorldPos(Vector3 gridPos)
     {
-        Vector3 worldPos = new Vector3(gridPos.x, -gridPos.y);
-        return worldPos;
+        return new Vector3(gridPos.x, -gridPos.y);
+    }
+
+    private bool isLerping()
+    {
+        return tweener.IsTweening(PacStudent.transform);
     }
 }
